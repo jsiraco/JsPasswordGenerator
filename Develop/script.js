f561b64252c351162a2dff3fc8f5246808bc70e5
Jas <ragnarock.josef@gmail.com>
2021-06-30 15:27:24 -0400

    Updated script.js

--- Develop/script.js
+++ Develop/script.js
@@ -3,15 +3,102 @@ let generateBtn = document.querySelector("#generate")
 let password = "TestPass";
 let passLength = 0;
 
-// Write password to the #password input
-let = writePassword = () => {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
+const upperCase = document.getElementById("lowerCase");
+const lowerCase = document.getElementById("upperCase");
+const number = document.getElementById("num");
+const specialChar = document.getElementById("specialChar");
 
-  passwordText.value = password;
 
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+
+slider.oninput = function () {
+  output.innerHTML = this.value;
 }
 
+generateBtn.addEventListener("click", () => {
+  const length = +myRange.value;
+  const hasUpper = upperCase.checked;
+  const hasLower = lowerCase.checked;
+  const hasNum = number.checked;
+  const hasSpecialChar = specialChar.checked;
+  
+  console.log(length, hasLower, hasNum, hasUpper, hasSpecialChar);
+  password = generatePassword(length, hasLower, hasNum, hasUpper, hasSpecialChar);
+});
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword(length, lower, number, upper, specialChar) {
+  let generatedPassword = "";
+  
+  const typeAmount = lower + number + upper + specialChar;
+  
+  console.log(typeAmount);
+  
+  const typesArray = [
+    { upper },
+    { lower },
+    { specialChar },
+    { number }].filter
+    (
+      item => Object.values(item)[0]
+      );
+      
+      console.log(typesArray);
+      
+      // if (typesAmount === 0) {
+      //   return "";
+      // }
+      
+      // for(let i = 0; i < length.length; i++) {
+        
+      // }
+    }
+    
+    let randomUpper = () => {
+      const upper = "QWERTYUIOPASDFGHJKLZXCVBNM";
+      return upper[Math.floor(Math.random() * upper.length)]
+    }
+    
+    let randomLower = () => {
+      const lower = "qwertyuiopasdfghjklzxcvbnm";
+      return lower[Math.floor(Math.random() * lower.length)]
+    }
+    
+    let randomNum = () => {
+      const num = "0123456789";
+      return num[Math.floor(Math.random() * num.length)]
+    }
+    
+    let randomSymbol = () => {
+      const symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+      return symbols[Math.floor(Math.random() * symbols.length)]
+    }
+
+    const randoms = {
+        lower: randomLower,
+        upper: randomUpper,
+        num: randomNum,
+        symbol: randomSymbol,
+    };
+    
+    let writePassword = () => {
+      const password = randoms.join();
+      console.log(password);
+      return password;
+    }
+    
+    // Write password to the #password input
+    let = writePassword = () => {
+      let password = generatePassword();
+      let passwordText = document.querySelector("#password");
+      
+      passwordText.value = password;
+      
+    }
+    
+    
+    // Add event listener to generate button
+    generateBtn.addEventListener("click", writePassword);
+    
